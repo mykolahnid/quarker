d53bfb8a8040a5dcd226214aef8792c5d6004408
mykolahnid <g.mykola@gmail.com>
2017-10-08 22:15:27 +0300

    facebook auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  displayName: string;
 
-  constructor() {
+  ngOnInit(): void {
+  }
+
+  constructor(public afAuth: AngularFireAuth) {
+  }
+
+  public login() {
+    console.log('before login');
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider);
+    console.log('after login');
+    this.displayName = this.afAuth.auth.currentUser.displayName;
+  }
+
+  public logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
